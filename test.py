5f891f163a9f91010537a4ef6b108fe546988ad5
MagEk1511 <ekamagerramov@gmail.com>
2025-09-23 21:07:41 +0300

    Добавлены тесты некорректных строк

--- test.py
+++ test.py
@@ -31,6 +31,36 @@ class TestChecker(unittest.TestCase):
         strings_to_check = ["0"]
         self.assertTrue(check_strings(strings_to_check, target_string_length))
 
+    def test_only_invalid_strings(self):
+        """Все строки некорректны"""
+        target_string_length = 2
+        strings_to_check = ["00", "00", "000"]
+        self.assertFalse(check_strings(strings_to_check, target_string_length))
+
+    def test_empty_when_not_zero_length(self):
+        """Пустой список для длины > 0"""
+        target_string_length = 3
+        strings_to_check = []
+        self.assertFalse(check_strings(strings_to_check, target_string_length))
+
+    def test_partial_overlap(self):
+        """Список содержит неполный набор"""
+        target_string_length = 3
+        strings_to_check = ["101", "010", "110"]
+        self.assertFalse(check_strings(strings_to_check, target_string_length))
+
+    def test_all_zeros_invalid(self):
+        """Строка полностью из нулей"""
+        target_string_length = 3
+        strings_to_check = ["000", "010", "101"]
+        self.assertFalse(check_strings(strings_to_check, target_string_length))
+
+    def test_two_zeros_together(self):
+        """Строка с двумя нулями подряд"""
+        target_string_length = 3
+        strings_to_check = ["001", "010", "011"]  # "001" некорректна
+        self.assertFalse(check_strings(strings_to_check, target_string_length))
+
 
 if __name__ == "__main__":
     unittest.main()
