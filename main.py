720d8f37976407ef2bca623d3c5de8f8baeb7be8
annie-hilator <anna.rashoyan@gmail.com>
2025-09-26 22:41:16 +0300

    added matrix generator and worked on recursive det

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from matrix_generator import generate_matrix_and_det
+
 def calculate_determinant(matrix: list[list[int]]) -> int:
+    # тут валидация
     """Вычисляет определитель целочисленной квадратной матрицы
 
     :param matrix: целочисленная квадратная матрица
@@ -6,17 +9,53 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    if not isinstance(matrix, list):
+        raise Exception("Something went wrong")
+
+    order = len(matrix)
+
+    # нужно проверить:
+    # что матрица квадратная (и строки одинаковой длины)
+    # что элементы числа
+
+    return _rec_det(matrix)
+
+def _rec_det(matrix: list[list[int]]) -> int:
+    '''
+    Рекурсивное вычисление определителя
+    '''
+    order = len(matrix)
+
+    if order == 1:
+        return matrix[0][0]
+    
+    det = 0
+    for idx, item in enumerate(matrix[0]):
+        det += item * (-1)**idx * _rec_det(_get_minor(0, idx, matrix))
+
+    return det
+
+def _get_minor(idx_row, idx_col, matrix):
+    '''
+    Возвращаем минор (матрицу без строки idx_row и столбца idx_col)
+    '''
+    return [[item for j, item in enumerate(row) if j != idx_col] for i, 
+            row in enumerate(matrix) if i != idx_row]
 
 
 def main():
     matrix = [[1, 2], [3, 4]]
     print("Матрица")
     for row in matrix:
-        print(row)
+        print(*row)
 
     print(f"Определитель матрицы равен {calculate_determinant(matrix)}")
 
+    print("\nПроверка генератора случайных матриц")
+    case = generate_matrix_and_det(4)
+    print("\n".join(["\t".join([str(cell) for cell in row]) for row in case.matrix]))
+    print("Заранее известный определитель матрицы:", case.det)
+    print("Определитель матрицы, вычисленный рекурсивно:", calculate_determinant(case.matrix))
 
 if __name__ == "__main__":
     main()
--- matrix_generator.py
+++ matrix_generator.py
@@ -15,11 +15,36 @@ def generate_matrix_and_det(order) -> Case:
     меньше 1
     :return: именованный кортеж Case с полями matrix, det
     """
-    pass
+    if not isinstance(order, int) or order < 1:
+        raise Exception("Order must be a positive integer")
 
+    # пока случайно создам определитель
+    det = random.randint(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
+    
+    # если порядок 1, то матрица из одного элемента (определителя)
+    if order == 1:
+        return Case(matrix=[[det]], det=det)
+    
+    # создаем единичную матрицу
+    matrix = [[1 if i == j else 0 for j in range(order)] for i in range(order)]
+
+    # первый элемент матрицы -- определитель
+    matrix[0][0] = det 
+    #print("Исходная:", matrix)
+
+    # меняем рандомные строки элементарными преобразованиями
+    # мб ещё что-нибудь покрутить тут
+    for _ in range(order * 2):
+        i, j = random.sample(range(order), 2)
+        rand_coef = random.randint(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE)
+        for col in range(order):
+            matrix[i][col] += rand_coef * matrix[j][col]
+        #print(f"Поменяли строку {i}:", matrix)
+
+    return Case(matrix=matrix, det=det)
 
 def main():
-    n = 10
+    n = 2
     print(f"Генерация матрицы порядка {n}")
     result = generate_matrix_and_det(n)
     print("\nОпределитель сгенерированной матрицы равен", result.det)
