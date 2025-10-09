3668e1892a38fff29543098e057ce0da6d2b553c
annie-hilator <anna.rashoyan@gmail.com>
2025-10-09 22:43:06 +0300

    solved the task

--- problems/knapsack_problem/brute_force_solver.py
+++ problems/knapsack_problem/brute_force_solver.py
@@ -2,12 +2,36 @@ from problems.knapsack_problem.knapsack_abs_solver import (
     KnapsackAbstractSolver,
     KnapsackSolution,
 )
+from itertools import combinations
 
 
 class BruteForceSolver(KnapsackAbstractSolver):
     def get_knapsack(self) -> KnapsackSolution:
-        """Решает задачу о рюкзаке с использованием полного перебора."""
-        pass
+        """
+        Решает задачу о рюкзаке с использованием полного перебора.
+
+        :return: максимальная возможнаая общая стоимость и список индексов выбранных предметов
+        :rtype: KnapsackSolution
+        """
+        _n = len(self._weights)
+        _best_cost: int = 0
+        _best_items: list[int] = []
+
+        # перебор всех комбинаций k предметов из n
+        for k in range(_n + 1):
+            # все возможные комбинации для каждого количества k
+            for comb in combinations(range(_n), k):
+                _items = list(comb)
+                _weight = self.get_weight(_items)
+                if _weight <= self._weight_limit:  # если есть место в рюкзаке...
+                    _cost = self.get_cost(_items)
+                    if (
+                        _cost > _best_cost and len(_items) > 0
+                    ):  # ...кладём предметы с макс. стоимостью
+                        _best_cost = _cost
+                        _best_items = _items
+
+        return KnapsackSolution(cost=_best_cost, items=_best_items)
 
 
 if __name__ == "__main__":
