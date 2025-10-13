bca18456fd922348718f2c4e3ca65c10624dd78d
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2025-10-14 01:24:53 +0500

    add task 6

---
+++ main.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+
+from strenum import StrEnum
+
+
+class ErrorMessages(StrEnum):
+    """Перечисление сообщений об ошибках."""
+
+    WRONG_MATRIX = (
+        "Таблица прибыли от проектов не является прямоугольной "
+        "матрицей с числовыми значениями"
+    )
+    NEG_PROFIT = "Значение прибыли не может быть отрицательно"
+    DECR_PROFIT = "Значение прибыли не может убывать с ростом инвестиций"
+
+
+Result = namedtuple("Result", ["profit", "distribution"])
+
+
+class ProfitValueError(Exception):
+    def __init__(self, message, project_idx, row_idx):
+        self.project_idx = project_idx
+        self.row_idx = row_idx
+        super().__init__(message)
+
+
+def get_invest_distribution(
+    profit_matrix: list[list[int]],
+) -> Result:
+    """Рассчитывает максимально возможную прибыль и распределение инвестиций
+    между несколькими проектами. Инвестиции распределяются кратными частями.
+
+    :param profit_matrix: Таблица с распределением прибыли от проектов в
+    зависимости от уровня инвестиций. Проекты указаны в столбцах, уровни
+    инвестиций в строках.
+    :raise ValueError: Если таблица прибыли от проектов не является
+    прямоугольной матрицей с числовыми значениями.
+    :raise ProfitValueError: Если значение прибыли отрицательно или убывает
+    с ростом инвестиций.
+    :return: именованный кортеж Result с полями:
+    profit - максимально возможная прибыль от инвестиций,
+    distribution - распределение инвестиций между проектами.
+    """
+    pass
+
+
+def main():
+    profit_matrix = [
+        [15, 18, 16, 17],
+        [20, 22, 23, 19],
+        [26, 28, 27, 25],
+        [34, 33, 29, 31],
+        [40, 39, 41, 37],
+    ]
+    print(get_invest_distribution(profit_matrix))
+
+
+if __name__ == "__main__":
+    main()
