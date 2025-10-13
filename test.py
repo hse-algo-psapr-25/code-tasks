0818a07e0f22374a82dc39f84ef0edc65b939986
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2025-10-14 00:43:47 +0500

    add task 6

---
+++ test.py
@@ -0,0 +1,151 @@
+import unittest
+
+from main import PARAM_ERR_MSG, Result, get_min_cost_path
+
+
+class TestTablePath(unittest.TestCase):
+    def test_none(self):
+        """Проверяет выброс исключения при передаче None в качестве параметра"""
+        self.assertRaisesRegex(ValueError, PARAM_ERR_MSG, get_min_cost_path, None)
+
+    def test_empty(self):
+        """Проверяет выброс исключения при передаче пустого списка в
+        качестве параметра"""
+        self.assertRaisesRegex(ValueError, PARAM_ERR_MSG, get_min_cost_path, [])
+
+    def test_empty_row(self):
+        """Проверяет выброс исключения при передаче списка с пустым списком в
+        качестве параметра"""
+        self.assertRaisesRegex(ValueError, PARAM_ERR_MSG, get_min_cost_path, [[]])
+
+    def test_str_value(self):
+        """Проверяет выброс исключения при наличии в матрице нечислового
+        значения"""
+        self.assertRaisesRegex(
+            ValueError, PARAM_ERR_MSG, get_min_cost_path, [[1, "ab"]]
+        )
+
+    def test_none_value(self):
+        """Проверяет выброс исключения при наличии в матрице значения None"""
+        self.assertRaisesRegex(
+            ValueError, PARAM_ERR_MSG, get_min_cost_path, [[1, None]]
+        )
+
+    def test_jag(self):
+        """Проверяет выброс исключения при наличии в матрице строк разной
+        длины"""
+        table = [[1.0, 2.0, 3.0], [1.0, 2.0]]
+        self.assertRaisesRegex(ValueError, PARAM_ERR_MSG, get_min_cost_path, table)
+
+    def test_single(self):
+        """Проверяет поиск кратчайшего пути в матрице размером 1*1"""
+        self.assertEqual(get_min_cost_path([[1]]), Result(cost=1.0, path=[(0, 0)]))
+
+    def test_double(self):
+        """Проверяет поиск кратчайшего пути в матрице размером 2*2"""
+        table = [[1.1, 2], [3, 4]]
+        self.assertEqual(
+            get_min_cost_path(table), Result(cost=7.1, path=[(0, 0), (0, 1), (1, 1)])
+        )
+
+    def test_negative(self):
+        """Проверяет поиск кратчайшего пути при наличии в матрице
+        отрицательного значения"""
+        table = [[1.1, 2], [-3, 4]]
+        self.assertEqual(
+            get_min_cost_path(table), Result(cost=2.1, path=[(0, 0), (1, 0), (1, 1)])
+        )
+
+    def test_triple(self):
+        """Проверяет поиск кратчайшего пути в матрице размером 3*3"""
+        table = [[1, 2, 2], [3, 4, 2], [1, 1, 2]]
+        self.assertEqual(
+            get_min_cost_path(table),
+            Result(cost=8.0, path=[(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]),
+        )
+
+    def test_rectangle(self):
+        """Проверяет поиск кратчайшего пути в прямоугольной матрице
+        размером 2*3"""
+        table = [[1, 2, 2], [3, 4, 1]]
+        self.assertEqual(
+            get_min_cost_path(table),
+            Result(cost=6.0, path=[(0, 0), (0, 1), (0, 2), (1, 2)]),
+        )
+
+    def test_square(self):
+        """Проверяет поиск кратчайшего пути в квадратной матрице размером 6*6"""
+        table = [
+            [1, 2, 2, 1, 3, 4],
+            [3, 1, 1, 5, 7, 6],
+            [3, 4, 1, 2, 7, 6],
+            [5, 7, 1, 6, 4, 4],
+            [5, 9, 2, 3, 5, 8],
+            [2, 2, 1, 3, 1, 6],
+        ]
+        self.assertEqual(
+            get_min_cost_path(table),
+            Result(
+                cost=20.0,
+                path=[
+                    (0, 0),
+                    (0, 1),
+                    (1, 1),
+                    (1, 2),
+                    (2, 2),
+                    (3, 2),
+                    (4, 2),
+                    (5, 2),
+                    (5, 3),
+                    (5, 4),
+                    (5, 5),
+                ],
+            ),
+        )
+
+    def test_rectangle_large(self):
+        """Проверяет поиск кратчайшего пути в прямоугольной матрице
+        размером 12*6"""
+        table = [
+            [8, 9, 2, 1, 6, 9],
+            [2, 3, 4, 8, 5, 1],
+            [4, 1, 7, 7, 1, 7],
+            [5, 6, 2, 8, 5, 6],
+            [3, 5, 2, 5, 8, 3],
+            [6, 9, 1, 3, 1, 5],
+            [7, 5, 4, 4, 2, 9],
+            [8, 7, 4, 1, 3, 5],
+            [6, 5, 7, 7, 6, 2],
+            [6, 2, 4, 8, 6, 3],
+            [7, 7, 2, 4, 5, 7],
+            [3, 8, 1, 6, 7, 1],
+        ]
+        self.assertEqual(
+            get_min_cost_path(table),
+            Result(
+                cost=52.0,
+                path=[
+                    (0, 0),
+                    (1, 0),
+                    (1, 1),
+                    (2, 1),
+                    (3, 1),
+                    (3, 2),
+                    (4, 2),
+                    (5, 2),
+                    (5, 3),
+                    (5, 4),
+                    (6, 4),
+                    (7, 4),
+                    (7, 5),
+                    (8, 5),
+                    (9, 5),
+                    (10, 5),
+                    (11, 5),
+                ],
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
