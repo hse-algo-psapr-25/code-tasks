2e171dff9e8cb352ed613ca44fc5779d906a2bf3
notikke <notikke@vk.com>
2025-10-20 21:25:05 +0300

    Completed task 6

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
 from collections import namedtuple
 
-# from strenum import StrEnum
+from strenum import StrEnum
 
 
-# class ErrorMessages(StrEnum):
-#     """Перечисление сообщений об ошибках."""
+class ErrorMessages(StrEnum):
+    """Перечисление сообщений об ошибках."""
 
-#     WRONG_MATRIX = (
-#         "Таблица прибыли от проектов не является прямоугольной "
-#         "матрицей с числовыми значениями"
-#     )
-#     NEG_PROFIT = "Значение прибыли не может быть отрицательно"
-#     DECR_PROFIT = "Значение прибыли не может убывать с ростом инвестиций"
+    WRONG_MATRIX = (
+        "Таблица прибыли от проектов не является прямоугольной "
+        "матрицей с числовыми значениями"
+    )
+    NEG_PROFIT = "Значение прибыли не может быть отрицательно"
+    DECR_PROFIT = "Значение прибыли не может убывать с ростом инвестиций"
 
 
 Result = namedtuple("Result", ["profit", "distribution"])
 
 
-# class ProfitValueError(Exception):
-#     def __init__(self, message, project_idx, row_idx):
-#         self.project_idx = project_idx
-#         self.row_idx = row_idx
-#         super().__init__(message)
+class ProfitValueError(Exception):
+    def __init__(self, message, project_idx, row_idx):
+        self.project_idx = project_idx
+        self.row_idx = row_idx
+        super().__init__(message)
 
 
-def get_invest_distribution(
-    profit_matrix: list[list[int]],
-):
+def _validate_profit_matrix(profit_matrix: list[list[int]]) -> None:
+    """Валадирует входную матрицу
+
+    Args:
+        profit_matrix (list[list[int]]): матрица распредления прибыли от инвестиций
+
+    Raises:
+        Соответсвующие ошибки
+    """
+    if not profit_matrix:
+        raise ValueError(ErrorMessages.WRONG_MATRIX)
+    
+    if not profit_matrix[0]:
+        raise ValueError(ErrorMessages.WRONG_MATRIX)
+    
+    n_cols = len(profit_matrix[0])
+    for i, row in enumerate(profit_matrix):
+        if len(row) != n_cols:
+            raise ValueError(ErrorMessages.WRONG_MATRIX)
+        
+        for j, value in enumerate(row):
+            if not isinstance(value, (int, float)):
+                raise ValueError(ErrorMessages.WRONG_MATRIX)
+            if value < 0:
+                raise ProfitValueError(ErrorMessages.NEG_PROFIT, j, i)
+            
+        if i > 0:
+            for j in range(n_cols):
+                if row[j] < profit_matrix[i-1][j]:
+                    raise ProfitValueError(ErrorMessages.DECR_PROFIT, j, i)
+
+
+def get_invest_distribution(profit_matrix: list[list[int]]):
     """Рассчитывает максимально возможную прибыль и распределение инвестиций
     между несколькими проектами. Инвестиции распределяются кратными частями.
 
@@ -41,60 +71,77 @@ def get_invest_distribution(
     profit - максимально возможная прибыль от инвестиций,
     distribution - распределение инвестиций между проектами.
     """
+
+    _validate_profit_matrix(profit_matrix)
+
+    max_profit, choices = _get_max_profits(profit_matrix)
+    distribution = _get_distribution(choices)
     
-    invest_count = len(profit_matrix)
-    max_profit = [[0] * invest_count]
-
-
-    max_profit = _get_max_profits(profit_matrix)
-
-    distribution = _get_distribution(max_profit)
     return Result(profit=max_profit[-1][-1], distribution=distribution)
 
-def _get_max_profits(profit_matrix):
-    
-    project_count = len(profit_matrix[0])
-    extend_profit_matrix = [[0] * project_count] + profit_matrix
-    project_cnt = len(extend_profit_matrix[0])
-    level_cnt = len(extend_profit_matrix)
 
-    max_profits = [[0] * project_cnt for _ in range(level_cnt)]
+def _get_max_profits(profit_matrix: list[list[int]]) -> tuple[list[list[int]], list[list[int]]]:
+    """Заполянет матрицу максимальной прибыли от вложений
+
+    Args:
+        profit_matrix (list[list[int]]): матрица прибыли от вложений
+
+    Returns:
+        tuple[list[list[int]], list[list[int]]]: матрица максимальной прибыли, матрица наших выборов для восстановления значения
+    """
+    n_projects = len(profit_matrix[0])
+    invest_levels = len(profit_matrix)
+    extend_matrix = [[0] * n_projects]
+    extend_matrix.extend(profit_matrix)
+    max_profits = [[0] * n_projects for _ in range(invest_levels + 1)]
+    choices = [[0] * n_projects for _ in range(invest_levels + 1)]
     
-    for level in range(level_cnt):
-        max_profits[level][0] = extend_profit_matrix[level][0]
+    for investment in range(invest_levels + 1):
+        max_profits[investment][0] = extend_matrix[investment][0]
+        choices[investment][0] = investment
+    
+    for project in range(1, n_projects):
+        for investment_level in range(invest_levels + 1):
+            best_profit, best_choice = 0, 0
+            for current_investment_level in range(investment_level + 1):
+                prev_level = investment_level - current_investment_level
+                prev_profit = max_profits[prev_level][project - 1]
+                current_profit = extend_matrix[current_investment_level][project]
+                current_total_profit = current_profit + prev_profit
+                if current_total_profit > best_profit:
+                    best_profit = current_total_profit
+                    best_choice = current_investment_level
+            max_profits[investment_level][project] = best_profit
+            choices[investment_level][project] = best_choice
+                
+    return max_profits, choices
+  
+  
+def _get_distribution(choices: list[list[int]]) -> list[int]:
+    """восстанавливает наше решение (выборы проектов вложения)
+
+    Args:
+        choices (list[list[int]]): матрица наших выборов по проектам
+
+    Returns:
+        list[int]: список с вложениями, где на соответствующем индексе стоит сколько мы туда вложим частей нашего бюджета
+    """
+    n_projects = len(choices[0])
+    total_investment = len(choices) - 1
+    
+    distribution = [0] * n_projects
+    remaining_invest = total_investment
+    
+    for project in range(n_projects - 1, 0, -1):
+        invest_to_proj = choices[remaining_invest][project]
+        distribution[project] = invest_to_proj
+        remaining_invest -= invest_to_proj
         
-    print(f"----------------------------НАЧАЛО---------------------------------")
-    print(extend_profit_matrix)
-    for proj_idx in range(project_cnt):
-        for level in range(level_cnt):
-            max_profit = 0
-            max_profit_cur_level = 0
-            print(f"Level: {level}")
-            for cur_level in range(level+1):
-                print(f"Cur Level: {cur_level}")
-                prev_level = level - cur_level
-                cur_profit = extend_profit_matrix[cur_level][proj_idx]
-                prev_profit = max_profits[prev_level][proj_idx - 1]
-                if cur_profit + prev_profit > max_profit:
-                    max_profit = cur_profit + prev_profit
-                    max_profit_cur_level = cur_level
-            max_profits[level][proj_idx] = max_profit
-            print(f"max_profits: {max_profits}")
-            
-    """
-              А   AB   ABC   ABCD
-        0:   [0,  0,   0,    0],
-        100: [15, 18,  18,   18],
-        200: [20, 33,  34,   35],
-        300: [26, 38,  49,   51],
-        400: [34, 44,  56,   66],
-        500: [40, 52,  61,   73],
-    """
-    print(max_profits)
-    return max_profits
+    distribution[0] = remaining_invest
+    
+    return distribution
 
-def _get_distribution(costs):
-    ...
+
 def main():
     profit_matrix = [
         [15, 18, 16, 17],
